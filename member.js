b4b027aad47210055921cb595814285eae36e4dc
cleisegang <cleisegang@gmail.com>
2023-07-30 17:29:27 +0000

    Copilot second commit.

--- member.js
+++ member.js
@@ -1,9 +1,27 @@
 function skillsmember() {
     var skills = document.getElementById("skills");
     var skillsvalue = skills.options[skills.selectedIndex].value;
-    if (skillsvalue == "yes") {
-        document.getElementById("skill").style.display = "block";
-    } else {
-        document.getElementById("skill").style.display = "none";
+    var skillsname = skills.options[skills.selectedIndex].text;
+    var skillslist = document.getElementById("skillslist");
+    var skillslistvalue = skillslist.value;
+    if (skillslistvalue.indexOf(skillsvalue) == -1) {
+        if (skillslistvalue == "") {
+            skillslist.value = skillsvalue;
+        } else {
+            skillslist.value = skillslistvalue + "," + skillsvalue;
+        }
+        var skillslistname = document.getElementById("skillslistname");
+        if (skillslistname.value == "") {
+            skillslistname.value = skillsname;
+        } else {
+            skillslistname.value = skillslistname.value + "," + skillsname;
+        }
+        var skillslistdiv = document.getElementById("skillslistdiv");
+        var skillslistdivhtml = skillslistdiv.innerHTML;
+        if (skillslistdivhtml == "") {
+            skillslistdiv.innerHTML = "<span class=\"label label-info\">" + skillsname + "</span>";
+        } else {
+            skillslistdiv.innerHTML = skillslistdivhtml + "<span class=\"label label-info\">" + skillsname + "</span>";
+        }
     }
 }
